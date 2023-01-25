37320c7c0ec96ddf32b6b665f981ba8fbff2a262
Guilherme Arruda <guipanfiglio123@outlook.com>
2023-01-25 17:57:52 -0300

    Update Caesars Cipher

--- Caesars Cipher/index.js
+++ Caesars Cipher/index.js
@@ -1,35 +1,35 @@
 const alphabet = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
 ];
 
 function rot13(str) {
-  let arr = str.split("");
-  let finalStr = "";
+  let arr = str.split('');
+  let finalStr = '';
   for (let i = 0; i < arr.length; i++) {
     if (alphabet.indexOf(arr[i]) === -1) {
       finalStr += arr[i];
@@ -42,4 +42,4 @@ function rot13(str) {
   return finalStr;
 }
 
-console.log(rot13("SERR PBQR PNZC"));
+console.log(rot13('SERR PBQR PNZC'));
