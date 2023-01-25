690e93527059632fde1cbb374be6a1f1a52b9fcd
Guilherme Arruda <guipanfiglio123@outlook.com>
2023-01-25 17:57:23 -0300

    Update Palindrome Checker

--- Palindrome Checker/index.js
+++ Palindrome Checker/index.js
@@ -1,9 +1,9 @@
 function palindrome(str) {
   const regex = /[\W_]/g;
-  const newStr = str.toLowerCase().replace(regex, "");
-  const reverStr = newStr.split("").reverse().join("");
+  const newStr = str.toLowerCase().replace(regex, '');
+  const reverStr = newStr.split('').reverse().join('');
   if (reverStr === newStr) return true;
   return false;
 }
 
-console.log(palindrome("eye"));
+console.log(palindrome('eye'));
